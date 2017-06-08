bbf60bd6b015b0a83aecb5b4c6dc2c856552c785
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-08 16:31:21 +0200

    Overhaul index.php
    We remove the mostly useless comments, and get rid of the access
    protection, which is not necessary anymore.

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
-/*
-CMSimple - Calendar Plugin (based on v. 0.5 from 2006 by Michael Svarrer)
-*/
 
-if ((!function_exists('sv')) || (strlen($sl) !== 2) || preg_match('/calendar'.DIRECTORY_SEPARATOR.'index.php/i', sv('PHP_SELF')))die('Access denied');
+/**
+ * Copyright 2005-2006 Michael Svarrer
+ * Copyright 2007-2008 Tory
+ * Copyright 2008      Patrick Varlet
+ * Copyright 2011      Holger Irmler
+ * Copyright 2011-2013 Frank Ziesing
+ * Copyright 2017      Christoph M. Becker
+ *
+ * This file is part of Calendar_XH.
+ *
+ * Calendar_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Calendar_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Calendar_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-
-/*function to display a calendar in the template or on a page*/
 function calendar($year = '', $month = '', $eventpage = '')
 {
     ob_start();
@@ -14,22 +31,12 @@ function calendar($year = '', $month = '', $eventpage = '')
     return ob_get_clean();
 }
 
-// ****************************************************************
-// *                  Display of the event list                   *
-// ****************************************************************
-
-//function to display the list of events on a page
 function events($month, $year, $end_month, $past_month)
 {
     ob_start();
     (new Calendar\EventListController($month, $year, $end_month, $past_month))->defaultAction();
     return ob_get_clean();
 }
-
-
-// ****************************************************************
-// *                  Next coming event as marquee                *
-// ****************************************************************
 
 function nextevent()
 {
@@ -38,17 +45,7 @@ function nextevent()
     return ob_get_clean();
 }
 
-
-
-
-// ****************************************************************
-// *            Backend, editing the event file                   *
-// ****************************************************************
-
-
-
-//function to edit the eventfile
-function EditEvents($editeventswidth)
+function editevents($editeventswidth)
 {
     ob_start();
     (new Calendar\EditEventsController($editeventswidth))->defaultAction();
