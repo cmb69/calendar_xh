cmb69/calendar_xh

commit accb191f9fa5df355c87d61c237a2e45c922d2fb

Remove unused variables

classes/EditEventsController.php
@@ -38,12 +38,9 @@ class EditEventsController extends Controller
 
     public function defaultAction()
     {
-        global $pth, $sl, $plugin;
-
         if (!$this->editeventswidth) {
             $this->editeventswidth = $this->conf['event-input_memberpages_narrow_medium_or_wide'];
         }
-        $imageFolder = "{$pth['folder']['plugins']}{$plugin}/images";
         $events = (new EventDataService)->readEvents();
 
         if (isset($_POST['action'])) {
@@ -79,7 +76,7 @@ class EditEventsController extends Controller
             $added = false;
 
             $newevent = array();
-            foreach ($event as $j => $i) {
+            foreach (array_keys($event) as $j) {
                 if (!isset($delete[$j]) || $delete[$j] == '') {
                     //Checking the date format. Some impossible dates can be given, but don't hurt.
                     $pattern = '/[\d\d\|\?{1-2}|\-{1-2}]\\' . $this->dpSeperator() . '\d\d\\'
@@ -151,7 +148,7 @@ class EditEventsController extends Controller
     //==========================================================
     private function eventForm($events, $editeventswidth)
     {
-        global $hjs, $pth, $sl, $plugin, $tx;
+        global $hjs, $pth, $sl, $plugin, $tx, $sn;
 
         $hjs .= '<script type="text/javascript" src="'
              .  $pth['folder']['plugins'] . $plugin . '/dp/datepicker.js">{ "lang":"'.$sl.'" }</script>'."\n";
@@ -168,7 +165,7 @@ class EditEventsController extends Controller
                 $columns = 8;
                 break;
             default:
-                $colums = 6;
+                $columns = 6;
         }
         $tableclass = "calendar_input_{$editeventswidth}";
 
@@ -176,7 +173,7 @@ class EditEventsController extends Controller
         $o .= "<input type=\"hidden\" value=\"saveevents\" name=\"action\">\n";
         $o .= "<table class=\"calendar_input $tableclass\">\n";
         $o .= "<tr>\n";
-        $o .= "<td colspan=\"$columns\"><input class=\"submit\" type=\"submit\" value=\""
+        $o .= "<td colspan=\"{$columns}\"><input class=\"submit\" type=\"submit\" value=\""
             . ucfirst($tx['action']['save']) . "\" name=\"send\"></td>\n";
         $o .= "<td style=\"text-align: right; width: 16px;\"><input type=\"image\" src=\""
             . $imageFolder . "/add.png\" style=\"width: 16px; height: 16px;\" name=\"add[0]\""
@@ -530,6 +527,9 @@ class EditEventsController extends Controller
 EOS;
     }
 
+    /**
+     * @SuppressWarnings(PHPMD.UnusedPrivateMethod)
+     */
     private function dateSort($a, $b)
     {
         $pattern = '!(.*)\\' . $this->dpSeperator() . '(.*)\\' . $this->dpSeperator() . '(.*)!';

classes/CalendarController.php
@@ -88,7 +88,6 @@ class CalendarController extends Controller
         $event_time_array           = array();
 
         $t                = '';
-        $event_dates      = '';
         $eventdates       = '';
         $event_date       = '';
         $event_date_start = '';
@@ -251,7 +250,7 @@ class CalendarController extends Controller
                 $dayofweek = 6;
             }
 
-            foreach ($event_year_array as $keys => $temp) {
+            foreach (array_keys($event_year_array) as $keys) {
                 if ($event_year_array[$keys] == $this->year
                     && $event_month_array[$keys] == $this->month
                     && $event_date_array[$keys] == $i

classes/EventDataService.php
@@ -143,13 +143,13 @@ class EventDataService
         fclose($fp);
         // change owner, group and permissions of new file to same as backup file
         if ($owner !== false) {
-            $chown = chown($eventfile, $owner);
+            chown($eventfile, $owner);
         }
         if ($group !== false) {
-            $chgrp = chgrp($eventfile, $group);
+            chgrp($eventfile, $group);
         }
         if ($permissions !== false) {
-            $chmod = chmod($eventfile, $permissions);
+            chmod($eventfile, $permissions);
         }
         return true;
     }

classes/EventListController.php
@@ -49,12 +49,6 @@ class EventListController extends Controller
     {
         global $plugin;
 
-        // 4 variables for bugtraking only
-        $month_start = $this->month;
-        $year_start = $this->year;
-        $end_month_start = $this->endMonth;
-        $past_month_start = $this->pastMonth;
-
         $month_input = isset($_GET['month']) ? htmlspecialchars($_GET['month']) : '';
         $month_input .= isset($_POST['month']) ? htmlspecialchars($_POST['month']) : '';
 
@@ -132,7 +126,7 @@ class EventListController extends Controller
             while (!feof($fp)) {
                 $line = fgets($fp, 4096);
                 //var_dump($line);
-                list($eventdates, $event, $location, $link, $event_time, $description) = explode(';', $line);
+                list($eventdates, $event, $location, $link, $event_time) = explode(';', $line);
                 list($event_date_start, $event_end_date, $event_end_time) = explode(',', $eventdates);
                 list($event_date, $event_month, $event_year) = explode($this->dpSeperator(), $event_date_start);
                 list($event_end_date, $event_end_month, $event_end_year)
@@ -220,7 +214,7 @@ class EventListController extends Controller
 
             asort($event_datetime_array);
 
-            foreach ($event_datetime_array as $keys => $temp) {
+            foreach (array_keys($event_datetime_array) as $keys) {
                 //=============================================
                 //here the case of birthday annoncements starts
                 //=============================================

classes/Plugin.php
@@ -41,7 +41,7 @@ class Plugin
 
     private function handleAdministration()
     {
-        global $o, $sl, $pth, $plugin_cf, $plugin_tx, $admin, $action, $plugin;
+        global $o, $plugin_cf, $plugin_tx, $admin, $action, $plugin;
 
         $eventfile = (new EventDataService)->getFilename();
         if (!is_file($eventfile)) {

commit 920daeab9b21dfb907b69c0febc64b990a5c6eab

Fix regression introduced with accb191
We have to leave the action attribute of the form element empty, so
that the query string is retained.

classes/EditEventsController.php
@@ -148,7 +148,7 @@ class EditEventsController extends Controller
     //==========================================================
     private function eventForm($events, $editeventswidth)
     {
-        global $hjs, $pth, $sl, $plugin, $tx, $sn;
+        global $hjs, $pth, $sl, $plugin, $tx;
 
         $hjs .= '<script type="text/javascript" src="'
              .  $pth['folder']['plugins'] . $plugin . '/dp/datepicker.js">{ "lang":"'.$sl.'" }</script>'."\n";
@@ -169,7 +169,7 @@ class EditEventsController extends Controller
         }
         $tableclass = "calendar_input_{$editeventswidth}";
 
-        $o = "<form method=\"POST\" action=\"$sn\">\n";
+        $o = "<form method=\"POST\" action=\"\">\n";
         $o .= "<input type=\"hidden\" value=\"saveevents\" name=\"action\">\n";
         $o .= "<table class=\"calendar_input $tableclass\">\n";
         $o .= "<tr>\n";