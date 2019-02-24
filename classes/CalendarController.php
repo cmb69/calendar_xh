6b9bd22bf9c913eb33f936f574981e08b8b79650
Christoph M. Becker <cmbecker69@gmx.de>
2019-02-24 13:39:46 +0100

    Bump copyright year

--- classes/CalendarController.php
+++ classes/CalendarController.php
@@ -6,7 +6,7 @@
  * Copyright 2008      Patrick Varlet
  * Copyright 2011      Holger Irmler
  * Copyright 2011-2013 Frank Ziesing
- * Copyright 2017-2018 Christoph M. Becker
+ * Copyright 2017-2019 Christoph M. Becker
  *
  * This file is part of Calendar_XH.
  *
