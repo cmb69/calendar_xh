2f1443dee7cb149099625b0b6f086718cbe0d457
Christoph M. Becker <cmbecker69@gmx.de>
2021-03-27 13:47:11 +0100

    Fix iCalendar reader's date parsing
    This is broken as of commit 1756987; a simple unit test could have
    caught that.

--- classes/ICalendarReader.php
+++ classes/ICalendarReader.php
@@ -157,7 +157,7 @@ class ICalendarReader
     private function parseDateTime($value)
     {
         if (preg_match('/^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})/', $value, $matches)) {
-            return ["$matches[3]{$this->separator}$matches[2]{$this->separator}$matches[1]", "$matches[4]:$matches[5]"];
+            return ["$matches[1]{$this->separator}$matches[2]{$this->separator}$matches[3]", "$matches[4]:$matches[5]"];
         }
         return false;
     }
