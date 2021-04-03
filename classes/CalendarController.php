cmb69/calendar_xh

commit d8fe7951dd2fe406c36e9f3ea247da9089afb1d3

Make Event and LocalDateTime properties non nullable
Having to deal with these nullable properties is a PITA.  Instead, we
normalize the event starts and ends on event creation right away.  This
may result in invalid events; for now we just silently ignore these,
what still seems to be better than having invalid events with "random"
behavior.

classes/Event.php
@@ -31,7 +31,7 @@ class Event
     /** @var LocalDateTime */
     private $start;
 
-    /** @var LocalDateTime|null */
+    /** @var LocalDateTime */
     private $end;
 
     /** @var string */
@@ -55,8 +55,9 @@ class Event
      * @param string $linkadr
      * @param string $linktxt
      * @param string $location
+     * @return self|null
      */
-    public function __construct(
+    public static function create(
         $datestart,
         $dateend,
         $starttime,
@@ -66,8 +67,47 @@ class Event
         $linktxt,
         $location
     ) {
-        $this->start = new LocalDateTime($datestart ?: null, $starttime ?: null);
-        $this->end = $dateend !== null && $dateend !== '' ? new LocalDateTime($dateend, $endtime) : null;
+        if (!$dateend) {
+            if ($endtime) {
+                return null;
+            }
+            $endtime = $starttime ? $starttime : "23:59";
+            if (($end = LocalDateTime::fromIsoString("{$datestart}T{$endtime}")) === null) {
+                return null;
+            }
+        } else {
+            if (!$endtime) {
+                $endtime = $starttime ? $starttime : "23:59";
+            }
+            if (($end = LocalDateTime::fromIsoString("{$dateend}T{$endtime}")) === null) {
+                return null;
+            }
+        }
+        if ($starttime === '') {
+            $starttime = "00:00";
+        }
+        if (($start = LocalDateTime::fromIsoString("{$datestart}T{$starttime}")) === null) {
+            return null;
+        }
+        return new self($start, $end, $event, $linkadr, $linktxt, $location);
+    }
+
+    /**
+     * @param string $event
+     * @param string $linkadr
+     * @param string $linktxt
+     * @param string $location
+     */
+    private function __construct(
+        LocalDateTime $start,
+        LocalDateTime $end,
+        $event,
+        $linkadr,
+        $linktxt,
+        $location
+    ) {
+        $this->start = $start;
+        $this->end = $end;
         $this->event = $event;
         $this->linkadr = $linkadr;
         $this->linktxt = $linktxt;
@@ -111,7 +151,7 @@ class Event
      */
     public function getDateEnd()
     {
-        return $this->end !== null ? $this->end->getDate() : null;
+        return $this->end->getDate();
     }
 
     /**
@@ -119,7 +159,7 @@ class Event
      */
     public function getEndTime()
     {
-        return $this->end !== null ? $this->end->getTime() : null;
+        return $this->end->getTime();
     }
 
     /**
@@ -135,7 +175,6 @@ class Event
      */
     public function getEndTimestamp()
     {
-        assert($this->end !== null);
         return mktime(0, 0, 0, $this->end->getMonth(), $this->end->getDay(), $this->end->getYear());
     }
 

classes/EditEventsController.php
@@ -83,13 +83,12 @@ class EditEventsController extends Controller
                 if (!$this->isValidDate($post['dateend'][$i])) {
                     $post['dateend'][$i] = '';
                 }
-                //Birthday should never have an enddate
-                if (trim($post['location'][$i]) === '###') {
-                    $post['dateend'][$i] = '';
-                }
                 /** @var string[] $args */
                 $args = array_column($post, $i);
-                $events[] = new Event(...$args);
+                $maybeEvent = Event::create(...$args);
+                if ($maybeEvent !== null) {
+                    $events[] = $maybeEvent;
+                }
             } else {
                 $deleted = true;
             }
@@ -154,7 +153,7 @@ class EditEventsController extends Controller
      */
     private function createDefaultEvent()
     {
-        return new Event(
+        $event = Event::create(
             date('Y-m-d'),
             '',
             '',
@@ -164,5 +163,7 @@ class EditEventsController extends Controller
             '',
             ''
         );
+        assert($event !== null);
+        return $event;
     }
 }

classes/EventDataService.php
@@ -82,7 +82,7 @@ class EventDataService
                     $endtime = null;
                 }
                 if ($datestart != '' && $event != '') {
-                    $result[] = new Event(
+                    $maybeEvent = Event::create(
                         $datestart,
                         $dateend,
                         $starttime,
@@ -92,6 +92,9 @@ class EventDataService
                         $linktxt,
                         $location
                     );
+                    if ($maybeEvent !== null) {
+                        $result[] = $maybeEvent;
+                    }
                 }
             }
             flock($stream, LOCK_UN);
@@ -164,7 +167,7 @@ class EventDataService
                     $linktxt = "{$linkadr};{$linktxt}";
                 }
                 if ($datestart != '' && $event != '') {
-                    $result[] = new Event(
+                    $maybeEvent = Event::create(
                         $datestart,
                         $dateend,
                         $starttime,
@@ -174,6 +177,9 @@ class EventDataService
                         $linktxt,
                         $location
                     );
+                    if ($maybeEvent !== null) {
+                        $result[] = $maybeEvent;
+                    }
                 }
             }
             flock($stream, LOCK_UN);

classes/ICalendarReader.php
@@ -87,7 +87,7 @@ class ICalendarReader
             if ($isInEvent) {
                 if ($line === 'END:VEVENT') {
                     $isInEvent = false;
-                    $this->events[] = new Event(
+                    $maybeEvent = Event::create(
                         $event['datestart'],
                         $event['dateend'],
                         $event['starttime'],
@@ -97,6 +97,9 @@ class ICalendarReader
                         '',
                         $event['location']
                     );
+                    if ($maybeEvent !== null) {
+                        $this->events[] = $maybeEvent;
+                    }
                 } else {
                     list($property, $value) = $this->parseLine($line);
                     switch ($property) {

classes/LocalDateTime.php
@@ -21,40 +21,62 @@
 
 namespace Calendar;
 
+use Exception;
+
 class LocalDateTime
 {
-    /** @var int|null */
-    private $year = null;
+    /** @var int */
+    private $year;
 
-    /** @var int|null */
-    private $month = null;
+    /** @var int */
+    private $month;
 
-    /** @var int|null */
-    private $day = null;
+    /** @var int */
+    private $day;
 
-    /** @var int|null */
-    private $hour = null;
+    /** @var int */
+    private $hour;
 
-    /** @var int|null */
-    private $minute = null;
+    /** @var int */
+    private $minute;
 
     /**
-     * @param string|null $date
-     * @param string|null $time
+     * @param string $string
+     * @return LocalDateTime|null
      */
-    public function __construct($date, $time)
+    public static function fromIsoString($string)
     {
-        if ($date !== null) {
-            list($year, $month, $day) = explode('-', $date);
-            $this->year = (int) $year;
-            $this->month = (int) $month;
-            $this->day = (int) $day;
+        $pattern = '/^(\d{4})(?:-)(\d{2})(?:-)(\d{2})(?:T)(\d{2})(?::)(\d{2})$/';
+        if (!preg_match($pattern, $string, $matches)) {
+            return null;
         }
-        if ($time !== null && $time !== '') {
-            list($hour, $minute) = explode(':', $time);
-            $this->hour = (int) $hour;
-            $this->minute = (int) $minute;
+        try {
+            return new self(
+                (int) $matches[1],
+                (int) $matches[2],
+                (int) $matches[3],
+                (int) $matches[4],
+                (int) $matches[5]
+            );
+        } catch (Exception $ex) {
+            return null;
         }
+    }
+
+    /**
+     * @param int $year
+     * @param int $month
+     * @param int $day
+     * @param int $hour
+     * @param int $minute
+     */
+    public function __construct($year, $month, $day, $hour, $minute)
+    {
+        $this->year = $year;
+        $this->month = $month;
+        $this->day = $day;
+        $this->hour = $hour;
+        $this->minute = $minute;
     }
 
     /**
@@ -73,7 +95,6 @@ class LocalDateTime
      */
     public function getYear()
     {
-        assert($this->year !== null);
         return $this->year;
     }
 
@@ -82,7 +103,6 @@ class LocalDateTime
      */
     public function getMonth()
     {
-        assert($this->month !== null);
         return $this->month;
     }
 
@@ -91,7 +111,6 @@ class LocalDateTime
      */
     public function getDay()
     {
-        assert($this->day !== null);
         return $this->day;
     }
 
@@ -100,7 +119,6 @@ class LocalDateTime
      */
     public function getHour()
     {
-        assert($this->hour !== null);
         return $this->hour;
     }
 
@@ -109,7 +127,6 @@ class LocalDateTime
      */
     public function getMinute()
     {
-        assert($this->minute !== null);
         return $this->minute;
     }
 
@@ -118,9 +135,6 @@ class LocalDateTime
      */
     public function getDate()
     {
-        if ($this->year === null || $this->month === null || $this->day === null) {
-            return '';
-        }
         return sprintf("%04d-%02d-%02d", $this->year, $this->month, $this->day);
     }
 
@@ -129,9 +143,6 @@ class LocalDateTime
      */
     public function getTime()
     {
-        if ($this->hour === null || $this->minute === null) {
-            return '';
-        }
         return sprintf("%02d:%02d", $this->hour, $this->minute);
     }
 
@@ -140,7 +151,7 @@ class LocalDateTime
      */
     public function getTimestamp()
     {
-        return mktime((int) $this->hour, (int) $this->minute, 0, (int) $this->month, (int) $this->day, (int) $this->year);
+        return mktime($this->hour, $this->minute, 0, $this->month, $this->day, $this->year);
     }
 
     /**

tests/unit/ICalendarReaderTest.php
@@ -30,8 +30,9 @@ class ICalendarReaderTest extends TestCase
         $subject = new ICalendarReader(__DIR__ . '/basic.ics', '-');
         $actual = $subject->read();
         $this->assertContainsOnlyInstancesOf(Event::class, $actual);
-        $this->assertSame(0, (new LocalDateTime("1997-07-14", "17:00"))->compare($actual[0]->getStart()));
-        $this->assertSame(0, (new LocalDateTime("1997-07-15", "03:59"))->compare($actual[0]->getEnd()));
+        // var_dump(new LocalDateTime(1997, 7, 15, 3, 59), $actual[0]->getEnd());
+        $this->assertSame(0, (new LocalDateTime(1997, 7, 14, 17, 0))->compare($actual[0]->getStart()));
+        $this->assertSame(0, (new LocalDateTime(1997, 7, 15, 3, 59))->compare($actual[0]->getEnd()));
         $this->assertSame("Bastille Day Party", $actual[0]->event);
         $this->assertSame("", $actual[0]->linkadr);
         $this->assertSame("", $actual[0]->linktxt);

commit bbe5fe3d3254a1656aaf9ab1affde442e73d1100

Partially fix refactoring regression
Apparently, commit d8fe795 caused quite some regression.  We partially
fix this; we need more useful abstractions to fix the rest.  This is
all still a big mess.

classes/CalendarController.php
@@ -190,8 +190,7 @@ class CalendarController extends Controller
             return false;
         }
         $today = mktime(0, 0, 0, $this->month, $day, $this->year);
-        $endDate = $event->getDateEnd();
-        if ($endDate === null) {
+        if ($event->getDateEnd() === $event->start->getDate()) {
             return $event->getStartTimestamp() === $today;
         }
         if ($this->conf['show_days_between_dates']) {
@@ -218,13 +217,13 @@ class CalendarController extends Controller
     {
         $titles = [];
         foreach ($events as $event) {
-            if ($event->getDateEnd() !== null) {
+            if ($event->getDateEnd() > $event->getDateStart()) {
                 $text = sprintf(
                     "%s %s %s %s",
                     $event->event,
                     $this->lang['event_date_till_date'],
-                    (string) $event->getDateEnd(),
-                    (string) $event->getEndTime()
+                    $event->getDateEnd(),
+                    $event->getEndTime()
                 );
             } else {
                 $text = $event->event;

classes/Event.php
@@ -149,7 +149,7 @@ class Event
     }
 
     /**
-     * @return string|null
+     * @return string
      */
     public function getDateEnd()
     {
@@ -157,7 +157,7 @@ class Event
     }
 
     /**
-     * @return string|null
+     * @return string
      */
     public function getEndTime()
     {

classes/EventListController.php
@@ -249,11 +249,11 @@ class EventListController extends Controller
     private function getEventRowView(Event $event)
     {
         $time = $event->getStartTime();
-        if (($end = $event->end) !== null) {
-            if (!$end->day) {
+        if ($event->end->getDate() > $event->start->getDate()) {
+            if (!$event->end->day) {
                 $time .= ' ' . $this->lang['event_time_till_time'];
             }
-            $time .= '<br>' . (string) $event->getEndTime();
+            $time .= '<br>' . $event->getEndTime();
         }
         return [
             'is_birthday' => false,
@@ -274,7 +274,8 @@ class EventListController extends Controller
     {
         $t = sprintf("%02d", $event->start->day);
         // if beginning and end dates are there, these are put one under the other
-        if (($end = $event->end) !== null) {
+        if ($event->getDateEnd() > $event->getDateStart()) {
+            $end = $event->end;
             if ($this->month != $end->month
                 || $this->year != $end->year
             ) {

classes/NextEventController.php
@@ -62,9 +62,9 @@ class NextEventController extends Controller
                 $nexteventtext = '';
             } elseif ($nextevent->start->getTimestamp() >= $now) {
                 $timestamp = $nextevent->start->getTimestamp();
-                if (($end = $nextevent->getDateEnd()) !== null) {
+                if (($nextevent->getDateEnd()) > $nextevent->getDateStart()) {
                     $nexteventtext = $this->lang['event_date_till_date'] . " " . '<br>'
-                        . $end . " " . (string) $nextevent->getEndTime();
+                        . $nextevent->getDateEnd() . " " . $nextevent->getEndTime();
                 } else {
                     $nexteventtext = '';
                 }