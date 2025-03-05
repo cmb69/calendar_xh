b3c5f77ee6cc6a3004c0df28400c06cf2f462040
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-05 17:41:51 +0100

    Back to dev

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
 use Calendar\Dic;
 
-const CALENDAR_VERSION = "2.4";
+const CALENDAR_VERSION = "2.5-dev";
 
 /** @return string|never */
 function calendar(int $year = 0, int $month = 0, string $eventpage = '')
