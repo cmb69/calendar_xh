30dbb8e351992c04918826e1bed64609d19e46f5
Christoph M. Becker <cmbecker69@gmx.de>
2025-04-07 01:27:34 +0200

    Prepare release

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,7 +21,7 @@
 
 const CMSIMPLE_XH_VERSION = "CMSimple_XH 1.7.5";
 const CMSIMPLE_URL = "http://example.com/";
-const CALENDAR_VERSION = "3.0-dev";
+const CALENDAR_VERSION = "2.5";
 
 require_once './vendor/autoload.php';
 
